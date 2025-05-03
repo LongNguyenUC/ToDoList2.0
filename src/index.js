b3d01895a39f4e66c54df2dea112161c81b9b97d
Long Nguyen <192123324+LongNguyenUC@users.noreply.github.com>
2025-05-03 16:23:25 -0400

    Add display of to dos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { Project, displayProjects } from "./project.js";
-
+import { toDo, displayToDos } from "./todos.js";
 // Default Projects
 // new Project("Home");
 // new Project("My Work");
@@ -12,5 +12,13 @@ let userProjects = [
   new Project("Startup"),
 ];
 
+// testing to-dos class and display
+let userToDosTest = [
+  new toDo("Write Some Code", "dummy-text", "dummy-text", "dummy-text"),
+  new toDo("Turn in Project", "dummy-text", "dummy-text", "dummy-text"),
+  new toDo("Buy Snacks", "dummy-text", "dummy-text", "dummy-text"),
+];
+
 displayProjects(userProjects);
+displayToDos(userToDosTest);
 console.log("hello!");
---
+++ src/todos.js
@@ -0,0 +1,21 @@
+const toDosContainer = document.querySelector(".to-dos");
+const addTaskButton = document.querySelector(".add-task-container");
+
+export class toDo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+export function displayToDos(toDos) {
+  for (const task of toDos) {
+    let taskContainer = document.createElement("div");
+    console.log(task);
+    taskContainer.textContent = task.title;
+
+    toDosContainer.insertBefore(taskContainer, addTaskButton);
+  }
+}
